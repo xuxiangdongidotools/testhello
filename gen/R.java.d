8dff3159d45aa36b582223d0784e8a47fa305847
xuxiangdongidotools <xuxiangdong@idotools.com>
2016-03-18 11:57:28 +0800

    change:1212

--- gen/R.java.d
+++ gen/R.java.d
@@ -1,9 +1,9 @@
-D:\TeamCity\testhello\testhello\gen\com\example\testled\R.java \
- : D:\TeamCity\testhello\testhello\res\drawable-hdpi\ic_launcher.png \
-D:\TeamCity\testhello\testhello\res\drawable-mdpi\ic_launcher.png \
-D:\TeamCity\testhello\testhello\res\drawable-xhdpi\ic_launcher.png \
-D:\TeamCity\testhello\testhello\res\drawable-xxhdpi\ic_launcher.png \
-D:\TeamCity\testhello\testhello\res\layout\activity_main.xml \
-D:\TeamCity\testhello\testhello\res\values\dimens.xml \
-D:\TeamCity\testhello\testhello\res\values\strings.xml \
-D:\TeamCity\testhello\testhello\bin\AndroidManifest.xml \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\gen\com\example\testled\R.java \
+ : D:\TeamCity\buildAgent\work\3de96e708f2408e\res\drawable-hdpi\ic_launcher.png \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\res\drawable-mdpi\ic_launcher.png \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\res\drawable-xhdpi\ic_launcher.png \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\res\drawable-xxhdpi\ic_launcher.png \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\res\layout\activity_main.xml \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\res\values\dimens.xml \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\res\values\strings.xml \
+D:\TeamCity\buildAgent\work\3de96e708f2408e\bin\AndroidManifest.xml \
