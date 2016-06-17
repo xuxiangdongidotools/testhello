1af029a350f785565f51e7a72f27cf0085bb7d27
xuxiangdongidotools <xuxiangdong@idotools.com>
2016-06-17 17:46:08 +0800

    change

--- bin/classes/R.java.d
+++
@@ -1,9 +0,0 @@
-D:\wksp\linshi\testled\gen\com\example\testled\R.java \
- : D:\wksp\linshi\testled\res\drawable-hdpi\ic_launcher.png \
-D:\wksp\linshi\testled\res\drawable-mdpi\ic_launcher.png \
-D:\wksp\linshi\testled\res\drawable-xhdpi\ic_launcher.png \
-D:\wksp\linshi\testled\res\drawable-xxhdpi\ic_launcher.png \
-D:\wksp\linshi\testled\res\layout\activity_main.xml \
-D:\wksp\linshi\testled\res\values\dimens.xml \
-D:\wksp\linshi\testled\res\values\strings.xml \
-D:\wksp\linshi\testled\bin\AndroidManifest.xml \
